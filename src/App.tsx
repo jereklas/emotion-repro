d61e3c9b7018f8c375f6ae3bee3067edbf716f53
Jeremy Klas <jeremyklas@gmail.com>
2021-09-29 17:37:47 -0700

    emotion repro showing stylesheets on develop build

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ThemeProvider, createTheme, Button, Typography, Switch } from "@mui/material";
+
+const light = createTheme();
+const dark = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
+  const [theme, setTheme] = useState(light);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div style={{ backgroundColor: theme.palette.background.default }}>
+        <Typography style={{ color: theme.palette.text.primary }}>Toggle Theme</Typography>
+        <Switch checked={theme === dark} onClick={() => setTheme(theme === light ? dark : light)} />
+        {new Array(10).fill(0).map((_, i) => (
+          <div key={i}>
+            <div style={{ display: "flex", margin: "5px" }}>
+              <Button variant="outlined" color="primary">
+                A
+              </Button>
+              <Button variant="outlined" color="secondary">
+                A
+              </Button>
+              <Button variant="outlined" color="info">
+                A
+              </Button>
+              <Button variant="outlined" color="error">
+                A
+              </Button>
+              <Button variant="outlined" color="warning">
+                A
+              </Button>
+              <Button variant="outlined" color="success">
+                A
+              </Button>
+            </div>
+            <div style={{ display: "flex", margin: "5px" }}>
+              <Button variant="contained" color="primary">
+                A
+              </Button>
+              <Button variant="contained" color="secondary">
+                A
+              </Button>
+              <Button variant="contained" color="info">
+                A
+              </Button>
+              <Button variant="contained" color="error">
+                A
+              </Button>
+              <Button variant="contained" color="warning">
+                A
+              </Button>
+              <Button variant="contained" color="success">
+                A
+              </Button>
+            </div>
+            <div style={{ display: "flex", margin: "5px" }}>
+              <Button variant="text" color="primary">
+                A
+              </Button>
+              <Button variant="text" color="secondary">
+                A
+              </Button>
+              <Button variant="text" color="info">
+                A
+              </Button>
+              <Button variant="text" color="error">
+                A
+              </Button>
+              <Button variant="text" color="warning">
+                A
+              </Button>
+              <Button variant="text" color="success">
+                A
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </ThemeProvider>
   );
 }
 
